a10ca9dae7d2a32f6bed6fd50d198559c38e7374
JFrancisOaten <30006148@students.southwales.ac.uk>
2020-10-31 16:49:58 +0000

    script added allowing buttons to work

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class mainMenu : MonoBehaviour
+{
+    public bool isNewGame;
+    public bool isSettings;
+    public bool isQuit;
+
+    public void OnMouseUp()
+    {
+        if (isNewGame)
+        {
+            SceneManager.LoadScene(1);
+        }
+        else if (isSettings)
+        {
+            SceneManager.LoadScene(2);
+        }
+        else if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+}
