d2dbc89e59bd6a5adb1eb8e8322aaf8652caeeb2
JFrancisOaten <30006148@students.southwales.ac.uk>
2020-11-02 19:46:53 +0000

    added walking animation to WASD and idle animation

---
+++ Assets/Scripts/ThirdPersonAnimations.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonAnimations : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            anim.Play("Walking");
+        }
+        else if (Input.GetKeyUp("w"))
+        {
+            anim.Play("Idle");
+        }
+        else if (Input.GetKeyDown("a"))
+        {
+            anim.Play("Walking");
+        }
+        else if (Input.GetKeyUp("a"))
+        {
+            anim.Play("Idle");
+        }
+        else if (Input.GetKeyDown("s"))
+        {
+            anim.Play("Walking");
+        }
+        else if (Input.GetKeyUp("s"))
+        {
+            anim.Play("Idle");
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            anim.Play("Walking");
+        }
+        else if (Input.GetKeyUp("d"))
+        {
+            anim.Play("Idle");
+        }
+    }
+}
--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -9,7 +9,7 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 6f;
+    public float Speed = 6f;
    
 
     public float turnSmoothTime = 0.1f;
@@ -29,7 +29,7 @@ public class ThirdPersonMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            controller.Move(moveDir.normalized * Speed * Time.deltaTime);
 
             
         }
