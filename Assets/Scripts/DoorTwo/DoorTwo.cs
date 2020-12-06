d79119a4fe7c6c4ab7ab89ccf141cdb114946c85
JFrancisOaten <jacko.1@live.co.uk>
2020-12-06 01:47:01 +0000

    added two pressure plate systems to the testingScene

---
+++ Assets/Scripts/DoorTwo/DoorTwo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTwo : MonoBehaviour
+{
+    public PlateOne plateOne; //Making a reference to the plateOne script
+    public PlateTwo plateTwo; //Making a reference to the plateTwo script
+    public PlateThree plateThree; //Making a reference to the plateThree script
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (plateOne.isPressed && plateTwo.isPressed && plateThree.isPressed)
+        {
+            transform.Translate(0, 2, 0);
+        }
+    }
+}
