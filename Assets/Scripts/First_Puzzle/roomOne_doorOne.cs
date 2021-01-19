c867fe9a47cbcdacff2698b417b56041730a8394
JFrancisOaten <jacko.1@live.co.uk>
2021-01-19 02:09:29 +0000

    added jumping puzzle with pressure plate

---
+++ Assets/Scripts/First_Puzzle/roomOne_doorOne.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomOne_doorOne : MonoBehaviour
+{
+    public PlateOne plateOne; //Making a reference to the plateOne script
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (plateOne.isPressed)
+        {
+            transform.Translate(0, 2, 0);
+        }
+    }
+}
