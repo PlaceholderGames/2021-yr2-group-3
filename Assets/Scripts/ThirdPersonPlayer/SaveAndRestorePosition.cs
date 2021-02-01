84f4e1b405e48bdd1397058a1e08bbe055950056
JFrancisOaten <jacko.1@live.co.uk>
2021-02-01 20:24:50 +0000

    saves the players position when they leave the scene so if they return they are in the correct location

---
+++ Assets/Scripts/ThirdPersonPlayer/SaveAndRestorePosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveAndRestorePosition : MonoBehaviour
+{
+    void Start() //checking for a saved position for this scene and go there if there is a saved position
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (SavedPositionManager.savedPositions.ContainsKey(sceneIndex))
+        {
+            transform.position = SavedPositionManager.savedPositions[sceneIndex];
+        }
+    }
+
+    void OnDestroy()
+    {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SavedPositionManager.savedPositions[sceneIndex] = transform.position;
+    }
+}
