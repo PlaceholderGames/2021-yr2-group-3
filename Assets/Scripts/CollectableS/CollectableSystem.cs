b7c08d12eaca31b97d033db83505ededd5a9e32f
PatrykTBS <18081851@students.southwales.ac.uk>
2021-04-22 18:55:15 +0100

    Added graveyard to the main map

---
+++ Assets/Scripts/CollectableS/CollectableSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectableSystem : MonoBehaviour
+{
+    public GameObject partsCollectedText;
+    public static int partsAmount = 0;
+
+    void Update()
+    {
+        partsCollectedText.GetComponent<Text>().text = "PARTS COLLECTED: " + partsAmount;
+        DontDestroyOnLoad(transform.gameObject); //Objects persists on scene loading
+    }
+}
