516d9e63fec4d3e8c1afe1a5eb82dffb93c5b5ba
JFrancisOaten <jacko.1@live.co.uk>
2021-04-23 17:09:52 +0100

    added changes to the collectable system and the credits scenes

---
+++ Assets/Scripts/CreditsButtons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsButtons : MonoBehaviour
+{
+    public int index;
+    public bool isCredits;
+    public bool isNextPage;
+    public bool isLastPage;
+
+    public void OnMouseUp()
+    {
+        if (isCredits)
+        {
+            SceneManager.LoadScene(index); //Loading level using build index
+        }
+        else if (isNextPage)
+        {
+            SceneManager.LoadScene(index);
+        }
+        else if (isLastPage)
+        {
+            SceneManager.LoadScene(index);
+        }
+    }
+
+
+}
